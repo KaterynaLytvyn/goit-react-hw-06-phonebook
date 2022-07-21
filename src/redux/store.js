6c0651235c4907a15a83f0e7a88785d9e67e1a48
Kateryna Lytvyn <sam.gilmore@gmail.com>
2022-07-21 21:41:09 +0300

    added saving to localstorage

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,36 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { contactsReducer } from './reducers'
+import {
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+  } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
+const persistConfig = {
+    key: 'phonebook',
+    version: 1,
+    storage,
+    blacklist: ['filter'],
+  };
+
+const persistedReducer = persistReducer(persistConfig, contactsReducer)
 
 export const store = configureStore({
     reducer: {
-        contacts: contactsReducer,
-    }
+        contacts: persistedReducer,
+    },
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware({
+        serializableCheck: {
+          ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+        },
+      }),
 })
+
+export const persistor = persistStore(store)
